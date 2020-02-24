f07c5e517d8938abd5bbd053142f0625df740358
Mohamed Salah Ahmed <mohamedsalahahmed@Mohameds-MacBook-Pro.local>
2020-02-24 13:06:18 +0100

    Added firebase.js and removed api key

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,36 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+// my test Api config
+var config = {
+/**
+ * ! Your firebase APi key
+ */
+};
+
+class Firebase {
+    constructor () {
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+       this.db = app.database();
+    }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
+
+}
+
+export default Firebase
