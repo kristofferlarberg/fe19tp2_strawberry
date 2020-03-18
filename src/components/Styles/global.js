1a6ddb61eb1e06c8bb6326eec5527884781aa475
Kristoffer Larberg <mail@kristofferlarberg.se>
2020-03-18 13:30:40 +0100

    customized scrollbar w darkmode and removed menu item

--- src/components/Styles/global.js
+++ src/components/Styles/global.js
@@ -16,16 +16,32 @@ export const GlobalStyles = createGlobalStyle`
     font-family: Roboto, sans-serif;
   }
 
+::-webkit-scrollbar {
+  width: 0.5rem;
+}
+
+::-webkit-scrollbar-track {
+  box-shadow: none; 
+  border: none;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.track};
+}
+ 
+::-webkit-scrollbar-thumb {
+  background: ${({ theme }) => theme.handle};
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: ${({ theme }) => theme.hover}; 
+}
+
   h1 {
     color: ${({ theme }) => theme.text};
   }
 
   h3 {
     color: ${({ theme }) => theme.h3};
-  }
-
-  span.Hyperlink {
-    color: ${({ theme }) => theme.text};
   }
 
   a, a:hover,a:active,a:visited {
